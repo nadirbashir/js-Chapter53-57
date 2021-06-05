fd6d577bd222a8dd1ca684e7b983727aec16896a
nadirbashir <1.nadirbashir@gmail.com>
2021-06-05 21:00:47 +0500

    task complete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 imgPaths = ["./images/1.jpg", "./images/2.jpg", "./images/3.jpg", "./images/4.png", "./images/5.jpg", "./images/6.jpg", "./images/7.png"]; 
 var imageBox = document.getElementById("images");
 for(var i = 0; i < imgPaths.length; i++){
-    imageBox.innerHTML += "<img src = '"+ imgPaths[i] +"'width = '200px' height = '200px' onClick = 'displayModal(this)'>"
+    imageBox.innerHTML += "<img class = 'images' src = '"+ imgPaths[i] +"' onClick = 'displayModal(this)'>"
 }
 
 function displayModal(x){
@@ -10,4 +10,13 @@ function displayModal(x){
     modal.classList.remove('modal-close');
     modal.style.display = 'block';
     document.getElementById("modal-img").src = x.src;
+}
+
+function onClosedImageModal(){
+    var modal = document.getElementById("modal");
+    modal.classList.add('modal-close');
+    modal.classList.remove('modal-open');
+    setTimeout(()=>{
+        modal.style.display = "none";
+    },550)
 }
